b2c3011ee4bf89cbc541a4edbd50d65464bcff86
Arthur Daussy <arthur.daussy@obeo.fr>
2026-02-16 14:37:16 +0100

    [releng] Add retry on cypress
    Before failing a cypress test, retry that test. It aims to provide
    stability on Gitlab CI of the Cypress tests.
    
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

--- integration-tests/cypress.config.ts
+++ integration-tests/cypress.config.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Obeo.
+ * Copyright (c) 2023, 2026 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -32,4 +32,5 @@ export default defineConfig({
     baseUrl: 'http://localhost:8080',
     experimentalRunAllSpecs: true,
   },
+  retries: 2,
 });
